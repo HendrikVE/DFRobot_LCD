1dd1eb8d6de83469aa795a6d920c928feb6c28e3
HendrikVE <hendrik1995@msn.com>
2020-03-09 13:38:38 +0100

    fix typos and code style

--- src/DFRobot_LCD.h
+++ src/DFRobot_LCD.h
@@ -12,22 +12,22 @@
  */
 
 
-#ifndef __DFRobot_LCD_H__
-#define __DFRobot_LCD_H__
+#ifndef DFRobot_LCD_H
+#define DFRobot_LCD_H
 
 #include <inttypes.h>
 #include "Print.h"
 
 /*!
- *  @brief Device I2C Arress
+ *  @brief Device I2C address
  */
-#define LCD_ADDRESS     (0x7c>>1)
-#define RGB_ADDRESS     (0xc0>>1)
+#define LCD_ADDRESS     (0x7c >> 1)
+#define RGB_ADDRESS     (0xc0 >> 1)
 
 
 /*!
- *  @brief color define
- */ 
+ *  @brief Color defines
+ */
 #define WHITE           0
 #define RED             1
 #define GREEN           2
@@ -44,7 +44,7 @@
 #define REG_OUTPUT      0x08
 
 /*!
- *  @brief commands
+ *  @brief Commands
  */
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
@@ -56,7 +56,7 @@
 #define LCD_SETDDRAMADDR 0x80
 
 /*!
- *  @brief flags for display entry mode
+ *  @brief Flags for display entry mode
  */
 #define LCD_ENTRYRIGHT 0x00
 #define LCD_ENTRYLEFT 0x02
@@ -64,7 +64,7 @@
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
 /*!
- *  @brief flags for display on/off control
+ *  @brief Flags for display on/off control
  */
 #define LCD_DISPLAYON 0x04
 #define LCD_DISPLAYOFF 0x00
@@ -74,7 +74,7 @@
 #define LCD_BLINKOFF 0x00
 
 /*!
- *  @brief flags for display/cursor shift
+ *  @brief Flags for display/cursor shift
  */
 #define LCD_DISPLAYMOVE 0x08
 #define LCD_CURSORMOVE 0x00
@@ -82,7 +82,7 @@
 #define LCD_MOVELEFT 0x00
 
 /*!
- *  @brief flags for function set
+ *  @brief Flags for function set
  */
 #define LCD_8BITMODE 0x10
 #define LCD_4BITMODE 0x00
@@ -91,140 +91,167 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS 0x00
 
-class DFRobot_LCD : public Print 
-{
+class DFRobot_LCD : public Print {
 
 public:
 
-  /*!
-   *  @brief Constructor
-   */
-  DFRobot_LCD(uint8_t lcd_cols,uint8_t lcd_rows,uint8_t lcd_Addr=LCD_ADDRESS,uint8_t RGB_Addr=RGB_ADDRESS);
-  
-  /*!
-   *  @brief initialize
-   */ 
-  void init();
-  
-  void clear();
-  void home();
+    /*!
+    *  @brief Constructor
+    */
+    DFRobot_LCD(uint8_t lcd_cols, uint8_t lcd_rows,
+                uint8_t lcd_Addr = LCD_ADDRESS,
+                uint8_t RGB_Addr = RGB_ADDRESS);
 
-  /*!
-   *  @brief Turn the display on/off (quickly)
-   */
-  void noDisplay();
-  void display();
+    /*!
+    *  @brief Initialize
+    */
+    void init();
 
-  /*!
-   *  @brief Turn on and off the blinking showCursor
-   */
-  void stopBlink();
-  void blink();
+    void clear();
 
-  /*!
-   *  @brief Turns the underline showCursor on/off
-   */
-  void noCursor();
-  void cursor();
+    void home();
 
-  /*!
-   *  @brief These commands scroll the display without changing the RAM
-   */
-  void scrollDisplayLeft();
-  void scrollDisplayRight();
- 
-  /*!
-   *  @brief This is for text that flows Left to Right
-   */
-  void leftToRight();
- 
-  /*!
-   *  @brief This is for text that flows Right to Left
-   */
-  void rightToLeft();
+    /*!
+    *  @brief Turn the display on/off (quickly)
+    */
+    void noDisplay();
 
-  /*!
-   *  @brief This will 'left justify' text from the showCursor
-   */
-  void noAutoscroll();
- 
-  /*!
-   *  @brief This will 'right justify' text from the showCursor
-   */
-  void autoscroll();
-   
-  /*!
-   *  @brief Allows us to fill the first 8 CGRAM locations
-   *		 with custom characters
-   */
-  void customSymbol(uint8_t, uint8_t[]);
-  void setCursor(uint8_t, uint8_t);  
-  
-  /*!
-   *  @brief color control
-   */
-  void setRGB(uint8_t r, uint8_t g, uint8_t b);               // set rgb
-  void setPWM(uint8_t color, uint8_t pwm){setReg(color, pwm);}      // set pwm 
-  void setColor(uint8_t color);
-  void setColorAll(){setRGB(0, 0, 0);}
-  void setColorWhite(){setRGB(255, 255, 255);}
+    void display();
 
-  /*!
-   *  @brief blink the LED backlight
-   */
-  void blinkLED(void);
-  void noBlinkLED(void);
+    /*!
+    *  @brief Turn on and off the blinking showCursor
+    */
+    void stopBlink();
 
-  /*!
-   *  @brief send data
-   */
-  virtual size_t write(uint8_t);
+    void blink();
 
-  /*!
-   *  @brief send command
-   */
-  void command(uint8_t);
-  
-  /*!
-   *  @brief compatibility API function aliases
-   */
-  void blink_on();						// alias for blink()
-  void blink_off();       					// alias for noBlink()
-  void cursor_on();      	 					// alias for cursor()
-  void cursor_off();      					// alias for noCursor()
-  void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-  void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
-  void printstr(const char[]);
-  
-  /*!
-   *  @brief Unsupported API functions (not implemented in this library)
-   */
-  uint8_t status();
-  void setContrast(uint8_t new_val);
-  uint8_t keypad();
-  void setDelay(int,int);
-  void on();
-  void off();
-  uint8_t init_bargraph(uint8_t graphtype);
-  void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-  void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-  
-  using Print::write;
-  
+    /*!
+    *  @brief Turns the underline showCursor on/off
+    */
+    void noCursor();
+
+    void cursor();
+
+    /*!
+    *  @brief These commands scroll the display without changing the RAM
+    */
+    void scrollDisplayLeft();
+
+    void scrollDisplayRight();
+
+    /*!
+    *  @brief This is for text that flows left to right
+    */
+    void leftToRight();
+
+    /*!
+    *  @brief This is for text that flows right to left
+    */
+    void rightToLeft();
+
+    /*!
+    *  @brief This will 'left justify' text from the showCursor
+    */
+    void noAutoscroll();
+
+    /*!
+    *  @brief This will 'right justify' text from the showCursor
+    */
+    void autoscroll();
+
+    /*!
+    *  @brief Allows us to fill the first 8 CGRAM locations
+    *         with custom characters
+    */
+    void customSymbol(uint8_t, uint8_t[]);
+
+    void setCursor(uint8_t, uint8_t);
+
+    /*!
+    *  @brief Color control
+    */
+    void setRGB(uint8_t r, uint8_t g, uint8_t b);               // set rgb
+    void setPWM(uint8_t color, uint8_t pwm) {
+        setReg(color, pwm);
+    }      // set pwm
+    void setColor(uint8_t color);
+
+    void setColorAll() { setRGB(0, 0, 0); }
+
+    void setColorWhite() { setRGB(255, 255, 255); }
+
+    /*!
+    *  @brief Blink the LED backlight
+    */
+    void blinkLED(void);
+
+    void noBlinkLED(void);
+
+    /*!
+    *  @brief Send data
+    */
+    virtual size_t write(uint8_t);
+
+    /*!
+    *  @brief Send command
+    */
+    void command(uint8_t);
+
+    /*!
+    *  @brief Compatibility API function aliases
+    */
+    void blink_on();      // alias for blink()
+    void blink_off();     // alias for noBlink()
+    void cursor_on();     // alias for cursor()
+    void cursor_off();    // alias for noCursor()
+    void
+    setBacklight(uint8_t new_val);   // alias for backlight() and nobacklight()
+    void load_custom_character(uint8_t char_num,
+                               uint8_t *rows);  // alias for createChar()
+    void printstr(const char[]);
+
+    /*!
+    *  @brief Unsupported API functions (not implemented in this library)
+    */
+    uint8_t status();
+
+    void setContrast(uint8_t new_val);
+
+    uint8_t keypad();
+
+    void setDelay(int, int);
+
+    void on();
+
+    void off();
+
+    uint8_t init_bargraph(uint8_t graphtype);
+
+    void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,
+                               uint8_t pixel_col_end);
+
+    void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,
+                             uint8_t pixel_col_end);
+
+    using Print::write;
+
 private:
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
-  void send(uint8_t *data, uint8_t len);
-  void setReg(uint8_t addr, uint8_t data);
-  uint8_t _showfunction;
-  uint8_t _showcontrol;
-  uint8_t _showmode;
-  uint8_t _initialized;
-  uint8_t _numlines,_currline;
-  uint8_t _lcdAddr;
-  uint8_t _RGBAddr;
-  uint8_t _cols;
-  uint8_t _rows;
-  uint8_t _backlightval;
+    void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+
+    void send(uint8_t *data, uint8_t len);
+
+    void setReg(uint8_t addr, uint8_t data);
+
+    uint8_t _showfunction;
+    uint8_t _showcontrol;
+    uint8_t _showmode;
+    uint8_t _initialized;
+    uint8_t _numlines, _currline;
+    uint8_t _lcdAddr;
+    uint8_t _RGBAddr;
+    uint8_t _cols;
+    uint8_t _rows;
+    uint8_t _backlightval;
 };
 
-#endif
+#endif /* DFRobot_LCD_H */
